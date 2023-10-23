5ea3fd7a57edb0be0fe9a83c48dd7b772edd0674
Nathanael Olsson <no222hj@student.lnu.se>
2023-10-23 22:23:32 +0200

    Edit logic to DRY.

--- src/js/classes/statistics-handler.js
+++ src/js/classes/statistics-handler.js
@@ -2,44 +2,27 @@ import { ChartsHandler } from "./charts-interface.js"
 
 export class StatisticsHandler {
 
-
-    constructor(expenseList, budgetList) {
-        this.expenseList = expenseList
-        this.budgetList = budgetList
-        this.totalExpense = this.#getTotalExpense()
-        this.totalBudget = this.#getTotalBudget()
-    }
-
-    generateExpenseDoughnutChart() {
-        const expenseDoughnut = this.generateDoughnutChart(this.expenseList)
-        const middleText = this.totalExpense
-        this.setUpChartToScaleParent(expenseDoughnut)
-        this.setUpDoughNutMiddleText(expenseDoughnut, middleText)
+    generateDoughnutCategoryChart(list) {
+        const expenseDoughnut = this.#generateDoughnutChart(list)
+        const middleText = this.#getTotalAmount(list)
+        this.#setUpChartToScaleParent(expenseDoughnut)
+        this.#setUpDoughNutMiddleText(expenseDoughnut, middleText)
 
         return expenseDoughnut
     }
 
-    generateBudgetDoughnutChart() {
-        const budgetDoughnut = this.generateDoughnutChart(this.budgetList)
-        const middleText = this.totalBudget
-        this.setUpChartToScaleParent(budgetDoughnut)
-        this.setUpDoughNutMiddleText(budgetDoughnut, middleText)
-
-        return budgetDoughnut
-    }
-
-    generateDoughnutChart(data) {
-        const chartsHandler = new ChartsHandler(data)
+    #generateDoughnutChart(list) {
+        const chartsHandler = new ChartsHandler(list)
         const doughnutChart = chartsHandler.getDougnutChart()
         return doughnutChart
     }
 
-    setUpChartToScaleParent(chart) {
+    #setUpChartToScaleParent(chart) {
         chart.setAttribute('width', '100%')
         chart.setAttribute('viewBox', '0 0 400 400')
     }
 
-    setUpDoughNutMiddleText(doughnutChart, middleText) {
+    #setUpDoughNutMiddleText(doughnutChart, middleText) {
         const doughnutChartText = document.createElementNS('http://www.w3.org/2000/svg', 'text')
         doughnutChartText.setAttribute('x', '50%')
         doughnutChartText.setAttribute('y', '50%')
@@ -51,26 +34,15 @@ export class StatisticsHandler {
         doughnutChart.appendChild(doughnutChartText)
     }
 
-    #getTotalBudget() {
-        let totalBudget = 0
-        for (const key in this.budgetList) {
-            if (Object.hasOwnProperty.call(this.budgetList, key)) {
-                const element = this.budgetList[key];
-                totalBudget += element.amount
+    #getTotalAmount(list) {
+        let totalAmount = 0
+        for (const key in list) {
+            if (Object.hasOwnProperty.call(list, key)) {
+                const element = list[key];
+                totalAmount += element.amount
             }
         }
-        return totalBudget
-    }
-
-    #getTotalExpense() {
-        let totalExpense = 0
-        for (const key in this.expenseList) {
-            if (Object.hasOwnProperty.call(this.expenseList, key)) {
-                const element = this.expenseList[key];
-                totalExpense += element.amount
-            }
-        }
-        return totalExpense
+        return totalAmount
     }
 
 }
